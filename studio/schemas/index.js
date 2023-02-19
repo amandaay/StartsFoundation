09219591429a4d5e94fe6d0c513676da1a36f058
yianan261 <yian.chen0131@gmail.com>
2023-02-19 14:14:55 +0800

    created schemas

---
+++ studio/schemas/index.js
@@ -0,0 +1,6 @@
+import customImage from "./objects/customImage";
+import blogs from "./documents/blogs";
+import gallery from "./documents/gallery";
+import news from "./documents/news";
+
+export const schemaTypes = [blogs,customImage,gallery,news]
