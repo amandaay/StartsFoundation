b1c755726e1fecdc12a2d0caa2ba60492f345de6
yianan261 <yian.chen0131@gmail.com>
2023-02-20 00:03:44 +0800

    deleted old cms file

--- client/cms/schemas/index.js
+++
@@ -1,6 +0,0 @@
-import blogs from './documents/blogs'
-import gallery from './documents/gallery'
-import news from './documents/news'
-import customImage from './objects/customImage'
-
-export const schemaTypes = [blogs, gallery, news, customImage]
