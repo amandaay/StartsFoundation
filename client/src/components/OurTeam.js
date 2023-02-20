40c8a095ae11c2aac34cb6bd359c4cf02be2a3c8
hauchu1998 <hchoward1998@gmail.com>
2023-02-20 00:22:09 -0500

    (OurTeam) show personal info when hovering pic

--- client/src/components/OurTeam.js
+++ client/src/components/OurTeam.js
@@ -3,10 +3,6 @@ import "../styles/OurTeam.css";
 import { StaticImage } from "gatsby-plugin-image"
 
 export function OurTeam() {
-    const [leftMember, setLeftMember] = useState("../images/about/about-2.jpg")
-    const [rightMembers, setRightMembers] = useState(["../images/about/about-2.jpg", "../images/about/about-2.jpg", "../images/about/about-2.jpg", "../images/about/about-2.jpg"])
-
-
     return (
         <div className="aboutMainDiv ">
             <h1 className="ourTeamBannerColor py-3">Our Team</h1>
@@ -20,22 +16,22 @@ export function OurTeam() {
             <div className="d-flex justify-content-center">
                 <div className="row gx-5 mt-4 p-4 bg-white w-75">
                     <div className="col-5 d-flex justify-content-center">
-                        <PersonalInfo image="../images/about/about-2.jpg" css="" ></PersonalInfo>
+                        <PersonalInfo image="../images/about/about-2.jpg" css="" imageCss="memberLeftPic"></PersonalInfo>
                     </div>
 
                     <div className="col-7">
-                        <div className="row gy-5">
-                            <PersonalInfo image="../images/about/about-2.jpg" css="col-6"></PersonalInfo>
-                            <PersonalInfo image="../images/about/about-2.jpg" css="col-6"></PersonalInfo>
-                            <PersonalInfo image="../images/about/about-2.jpg" css="col-6"></PersonalInfo>
-                            <PersonalInfo image="../images/about/about-2.jpg" css="col-6"></PersonalInfo>
+                        <div className="row g-5">
+                            <PersonalInfo image="../images/about/about-2.jpg" css="col-6" imageCss="memberRightPic"></PersonalInfo>
+                            <PersonalInfo image="../images/about/about-2.jpg" css="col-6" imageCss="memberRightPic"></PersonalInfo>
+                            <PersonalInfo image="../images/about/about-2.jpg" css="col-6" imageCss="memberRightPic"></PersonalInfo>
+                            <PersonalInfo image="../images/about/about-2.jpg" css="col-6" imageCss="memberRightPic"></PersonalInfo>
                         </div>
                     </div>
                 </div>
             </div>
 
 
-            <div className="mt-3 d-flex justify-content-center">
+            <div className="mt-4 d-flex justify-content-center">
                 <div className="ourTeamText bg-white text-center fs-5 col-12">
                     Test -
                 </div>
@@ -49,15 +45,28 @@ export function OurTeam() {
 function PersonalInfo(props) {
     const [showInfo, setShowInfo] = useState(false)
 
-    console.log(props.image)
+    // console.log(props.image)
     return (
-        <div className={props.css} onMouseEnter={() => setShowInfo(true)} onMouseLeave={() => setShowInfo(false)}>
-            <StaticImage
-                className="memberLeftPic"
-                src="../images/about/about-2.jpg"
-                alt="nepali young girls"
-            >
-            </StaticImage>
+        <div className={`${props.css}`} onMouseEnter={() => setShowInfo(true)} onMouseLeave={() => setShowInfo(false)}>
+            <div className="position-relative h-100">
+                <StaticImage
+                    className={props.imageCss}
+                    src="../images/about/about-2.jpg"
+                    alt="nepali young girls"
+                >
+                </StaticImage>
+                {
+                    showInfo && (
+                        <div className="bg-black position-absolute top-0 start-0 w-100 h-100 memberIntro px-3 pb-3 d-flex align-items-end">
+                            <div className="text-white">
+                                <div className="block w-100">description</div>
+                                <div className="fs-3 font-weight-bold">RAM KC</div>
+                                <div className="fs-5">Writing & Communications</div>
+                            </div>
+                        </div>
+                    )
+                }
+            </div>
 
         </div>
 
