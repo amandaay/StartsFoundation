850585442c090326792072afe21159b62c9c8254
pochun.huang1107@gmail.com <casper@Po-Chuns-MacBook-Pro.local>
2023-02-25 23:05:04 +0800

    renaming section and added bottom section

--- client/src/pages/donation.js
+++ client/src/pages/donation.js
@@ -6,27 +6,32 @@ import * as styles from '../styles/donation.module.css'
 export default function donation() {
     return (
         <Layout>
-            <section>
-                <div className={styles.banner}>
-                    <div className={styles.container}>
-                        <div className={styles.midPage}>
-                            <div className={styles.donationBox}>
-                                <div className={styles.donationForm}>
-                                    <h1>Donate Now</h1>
-                                    <div className={styles.subtitle}>Join Starts Foundation to save and support the life of people in Nepal.</div>
-                                    <DonationForm />
-                                </div>
+            <section className={styles.banner}>
+                <div className={styles.container}>
+                    <div className={styles.firstSection}>
+                        <div className={styles.donationBox}>
+                            <div className={styles.donationForm}>
+                                <h1>Donate Now</h1>
+                                <div className={styles.subtitle}>Join Starts Foundation to save and support the life of people in Nepal.</div>
+                                <DonationForm />
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className={styles.container}>
-                    <div className={styles.bottomPage}>
-                        <div className={styles.bottomContent}>
-                            <h1>TRANSPARENCY AND ACCOUNTABILITY</h1>
-                            <p>It has always maintained a transparent relationship with the development partners, community group, government and people associated all the while fostering the bond in a significant way. Standing accountable to all the terms, it has paved its way into society as one of the emerging social organizations which works to thrive with dignity.</p>
-                        </div>
+            </section>
+            <section className={styles.container}>
+                <div className={styles.secondSection}>
+                    <div className={styles.secondSectionContent}>
+                        <h1>AID FOR LIVING LIFE</h1>
+                        <p>In Nepal, unexpected disasters strip many people from the stable foundations through which they live and survive. These adversities devastate their physical and psychological well being as well as their means of sustenance.</p>
                     </div>
+                </div>
+            </section>
+            <section className={styles.container}>
+                <div className={styles.thirdSection}>
+                    <p>Our journey of Aid For Living Life began with the vision of providing expeditious aid to those struck by unexpected adverse circumstances in Nepal.</p>
+                    <p>In 2017, two people - one Nepali and one Norwegian, connected together in a time when floods were ravaging this nation and disrupting homes and lives of many. They had a vision to provide immediate needed support to those in need and so, the Aid For Living Life Project commenced by distributing relief packages to about 200 affected families.</p>
+                    <p>We are going strong since, and have recently distributed about 900 relief packages in the face of the COVID-19 pandemic.</p>
                 </div>
             </section>
         </Layout>
