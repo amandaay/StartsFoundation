e1719d6d9fcad1dd972e4bc3bab01fd719d75dfc
Po-Chun Huang <po-chunhuang@Po-Chuns-Mac-mini.local>
2023-03-05 10:02:09 +0800

    more responsiveness to the donation page

--- client/src/components/DonationForm.js
+++ client/src/components/DonationForm.js
@@ -1,20 +1,12 @@
 import React, { useState } from 'react'
+import { IoIosArrowBack } from 'react-icons/io'
+import { GoChevronDown, GoChevronLeft } from 'react-icons/go'
 import * as styles from '../styles/donationForm.module.css'
 
 export default function DonationForm() {
     // amount options
-    const monthlyAmount = [
-        "USD10",
-        "USD25",
-        "USD50",
-        "Other"
-    ]
-    const onetimeAmount = [
-        "USD30",
-        "USD50",
-        "USD100",
-        "Other"
-    ]
+    const monthlyAmount = ["USD10","USD25","USD50","Other"];
+    const onetimeAmount = ["USD30","USD50","USD100","Other"];
     // control between Donate Once and Donate Monthly
     const [donateMonthly, setDonateMonthly] = useState(false);
     function handleClick(e) {
@@ -49,12 +41,26 @@ export default function DonationForm() {
     });
     const renderedOtherAmount = (
         <div className={styles.otherAmount}>
-            <label for='number' className={styles.label}>USD</label>
-            <input name='number' type='number' className={styles.input} />
+            <label htmlFor='number' className={styles.label}>USD</label>
+            <input id='number' type='number' className={styles.input} />
         </div>
     )
-    return (
-        <section className={styles.container}>
+    // donate steps control
+    const [showFormDetails, setShowFormDetails] = useState(false);
+    function onDonateClick() {
+        setShowFormDetails(true);
+    }
+    const [showSummary, setShowSummary] = useState(true);
+    function onSummaryTabClick() {
+        setShowSummary(!showSummary);
+    }
+    const tabIcon = (
+        <span className="text-2xl">
+            {showSummary ? <GoChevronDown /> : <GoChevronLeft />}
+        </span>
+    );
+    const stepOne = (
+        <div>
             <div className={styles.options}>
                 <div role="presentation" onClick={handleClick} className={donateMonthly ? styles.frequencyUnselected : styles.frequencySelected}>Donate Once</div>
                 <div role="presentation" onClick={handleClick} className={!donateMonthly ? styles.frequencyUnselected : styles.frequencySelected}>Donate Monthly</div>
@@ -62,11 +68,30 @@ export default function DonationForm() {
             <div className={styles.amountOptionsBox}>
                 {donateMonthly ? renderedMonthlyAmounts : renderedOnetimeAmounts}
             </div>
+        </div>
+    )
+    const stepTwo = (
+        <div className={styles.detailedForm}>
+            <div>
+                <div className={styles.summaryTab} onClick={onSummaryTabClick}>Donation Summary {tabIcon}</div>
+                {showSummary&&<div className={styles.donationSummary}>
+                    <div>Frequency: {donateMonthly?"Monthly":"One time"}</div>
+                    <div>Amount: {donateMonthly?monthlyAmount[monthlyAmountSelected]:onetimeAmount[onetimeAmountSelected]}</div>
+                </div>}
+            </div>
+        </div>
+    )
+    return (
+        <section className={styles.container}>
+            {showFormDetails?stepTwo:stepOne}
             <div className={styles.submitForm}>
-                <div className={styles.subtitle}>As you proceed, you will be taken to a secure payment page, where you perform the transfer of your gift.</div>
+                {showFormDetails?
+                    <p className={styles.back} onClick={()=>setShowFormDetails(false)}><IoIosArrowBack />Back</p>:
+                    <div className={styles.subtitle}>As you proceed, you will be taken to a secure payment page, where you perform the transfer of your gift.
+                </div>}
                 <div className={styles.form}>
                     {((onetimeAmountSelected===3 && !donateMonthly)||(monthlyAmountSelected===3 && donateMonthly))&&renderedOtherAmount}
-                    <button className={styles.btn}>Donate Now</button>
+                    <button onClick={onDonateClick} className={styles.btn}>Donate Now</button>
                 </div>
             </div>
         </section>
