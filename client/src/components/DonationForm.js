9d21a6d77a736027b42f339f762b1c8f488eef1a
pochunhuang1107 <po-chunhuang@Po-Chuns-Mac-mini.local>
2023-03-07 23:40:22 +0800

    added second step of the donate form

--- client/src/components/DonationForm.js
+++ client/src/components/DonationForm.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
 import { IoIosArrowBack } from 'react-icons/io'
 import { GoChevronDown, GoChevronLeft } from 'react-icons/go'
+import PhoneInput from 'react-phone-number-input'
 import * as styles from '../styles/donationForm.module.css'
+import '../styles/phone.css'
 
 export default function DonationForm() {
     // amount options
@@ -24,6 +26,11 @@ export default function DonationForm() {
     function handleOnetimeAmountClick(index) {
         setOnetimeAmountSelected(index);
     }
+    const [donatedAmount, setDonatedAmount] = useState(0);
+    const [showFormDetails, setShowFormDetails] = useState(false);
+    function onInputChange(e) {
+        setDonatedAmount(e.target.value);
+    }
 
     const renderedMonthlyAmounts = monthlyAmount.map((amount, index) => {
         return (
@@ -41,14 +48,26 @@ export default function DonationForm() {
     });
     const renderedOtherAmount = (
         <div className={styles.otherAmount}>
-            <label htmlFor='number' className={styles.label}>USD</label>
-            <input id='number' type='number' className={styles.input} />
+            <label htmlFor='number' className={styles.currency}>USD</label>
+            <input id='number' type='number' value={donatedAmount} onChange={onInputChange} className={styles.amountInput} />
         </div>
     )
     // donate steps control
-    const [showFormDetails, setShowFormDetails] = useState(false);
+    const [donateClicked, setDonateClicked] = useState(false);
     function onDonateClick() {
+        let amount = 0;
         setShowFormDetails(true);
+        if(donateMonthly){
+            amount = monthlyAmount[monthlyAmountSelected].replace(/\D/g,'');
+        }else{
+            amount = onetimeAmount[onetimeAmountSelected].replace(/\D/g,'');
+        }
+        if(amount!==""){
+            setDonatedAmount(Number(amount));
+        }
+        setMonthlyAmountSelected(monthlyAmount.length-1);
+        setOnetimeAmountSelected(onetimeAmount.length-1);
+        setDonateClicked(true);
     }
     const [showSummary, setShowSummary] = useState(true);
     function onSummaryTabClick() {
@@ -70,14 +89,46 @@ export default function DonationForm() {
             </div>
         </div>
     )
+    const [phoneNumber, setPhoneNumber] = useState()
+    const [checked, setChecked] = useState(false)
     const stepTwo = (
         <div className={styles.detailedForm}>
-            <div>
-                <div className={styles.summaryTab} onClick={onSummaryTabClick}>Donation Summary {tabIcon}</div>
+            <div className={styles.summary}>
+                <div role='presentation' className={styles.summaryTab} onClick={onSummaryTabClick}>Donation Summary {tabIcon}</div>
                 {showSummary&&<div className={styles.donationSummary}>
                     <div>Frequency: {donateMonthly?"Monthly":"One time"}</div>
-                    <div>Amount: {donateMonthly?monthlyAmount[monthlyAmountSelected]:onetimeAmount[onetimeAmountSelected]}</div>
+                    <div>Amount: USD{donatedAmount}</div>
                 </div>}
+            </div>
+            <div>
+                <form className={styles.form}>
+                    <div className={styles.row}>
+                        <div className={styles.col}>
+                            <label htmlFor='firstName'>First name<span className={styles.span}> *</span></label>
+                            <input className={styles.formInput} name='firstName' required/>
+                        </div>
+                        <div className={styles.col}>
+                            <label htmlFor='lastName'>Last name<span className={styles.span}> *</span></label>
+                            <input className={styles.formInput} name='lastName' required/>
+                        </div>
+                    </div>
+                    <div className={styles.row}>
+                        <label htmlFor='email'>Email address<span className={styles.span}> *</span></label>
+                    </div>
+                    <div className={styles.row}>
+                        <input className={styles.formInput} type='email' name='email' required/>
+                    </div>
+                    <div role='presentation' className={styles.row} onClick={()=>setChecked(!checked)} style={{cursor: 'pointer', userSelect: 'none'}}>
+                        <input className={styles.checkBox} type='checkbox' checked={checked}/>
+                        <p className={styles.checkBoxContent}>Yes, I would like to receive communications by email and know how my donation is driving change in the lives of Nepal. By opting out, you will not be added to any Starts mailing lists. You will, however, still receive your donation receipt by email.</p>
+                    </div>
+                    <div className={styles.row}>
+                        <label htmlFor='phone'>Phone number</label>
+                    </div>
+                    <div className={styles.row}>
+                        <PhoneInput international defaultCountry="US" value={phoneNumber} onChange={()=>setPhoneNumber}/>
+                    </div>
+                </form>
             </div>
         </div>
     )
@@ -86,11 +137,11 @@ export default function DonationForm() {
             {showFormDetails?stepTwo:stepOne}
             <div className={styles.submitForm}>
                 {showFormDetails?
-                    <p className={styles.back} onClick={()=>setShowFormDetails(false)}><IoIosArrowBack />Back</p>:
+                    <p role='presentation' className={styles.back} onClick={()=>{setShowFormDetails(false);setDonateClicked(false);}}><IoIosArrowBack />Back</p>:
                     <div className={styles.subtitle}>As you proceed, you will be taken to a secure payment page, where you perform the transfer of your gift.
                 </div>}
                 <div className={styles.form}>
-                    {((onetimeAmountSelected===3 && !donateMonthly)||(monthlyAmountSelected===3 && donateMonthly))&&renderedOtherAmount}
+                    {(((onetimeAmountSelected===3 && !donateMonthly)||(monthlyAmountSelected===3 && donateMonthly))&&!donateClicked)&&renderedOtherAmount}
                     <button onClick={onDonateClick} className={styles.btn}>Donate Now</button>
                 </div>
             </div>
