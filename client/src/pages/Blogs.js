a773732e1dc4df78c84790111f97a281a42f7ab3
hauchu1998 <hchoward1998@gmail.com>
2023-03-15 17:06:02 -0400

    create pagination for blogs

---
+++ client/src/pages/Blogs.js
@@ -0,0 +1,16 @@
+import { navigate } from "gatsby"
+import PropTypes from "prop-types"
+
+function Blogs({ location }) {
+    console.log("Location", location)
+
+    location.pathname === "/Blogs/"
+        ? navigate("/Blogs/page=1")
+        : navigate(location.pathname)
+
+    return
+}
+Blogs.propTypes = {
+    location: PropTypes.object,
+}
+export default Blogs
