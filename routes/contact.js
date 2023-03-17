a3103d7c2a2cc527ae9650d0c96fff3f394c57e1
kasivisu4 <kasivisu4@gmail.com>
2023-03-17 09:11:14 -0700

    Removed package.json conflict and added the forms

--- routes/contact.js
+++ routes/contact.js
@@ -4,32 +4,34 @@ const router = express.Router();
 
 // Ref for Mail https://lo-victoria.com/how-to-build-a-contact-form-with-javascript-and-nodemailer
 router.post("/contact", async (req, res) => {
-  let transporter = await nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      type: "OAuth2",
-      clientId: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
-    },
-  });
+  try {
+    let transporter = await nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        type: "OAuth2",
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+      },
+    });
 
-  transporter.sendMail({
-    from: process.env.FROM_EMAIL,
-    to: `${process.env.FROM_EMAIL},${req.body.email}`,
-    subject: `Contact us | from: ${req.body.fName}`,
-    text: req.body.message,
-    auth: {
-      user: process.env.FROM_EMAIL,
-      refreshToken: process.env.REFRESH_TOKEN,
-      accessToken: process.env.ACCESS_TOKEN,
-    },
-  });
+    await transporter.sendMail({
+      from: process.env.FROM_EMAIL,
+      to: `${process.env.FROM_EMAIL},${req.body.email}`,
+      subject: `Contact us | from: ${req.body.fName}`,
+      text: req.body.message,
+      auth: {
+        user: process.env.FROM_EMAIL,
+        refreshToken: process.env.REFRESH_TOKEN,
+        accessToken: process.env.ACCESS_TOKEN,
+      },
+    });
 
-  console.log("contact request n");
-  console.log(req.body);
-  res.status(200).json({ success: true });
+    res.status(200).json({ mailSent: true });
+  } catch {
+    res.status(200).json({ mailSent: false });
+  }
 });
 
 router.get("/", (req, res) => {
