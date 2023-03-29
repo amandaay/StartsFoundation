f386da1da1fe8e32dadddb3b05105685e7542f2b
yianan261 <yian.chen0131@gmail.com>
2023-03-29 15:00:59 -0400

    updated screen responsiveness for objectives, recent news

--- client/src/components/Objectives.js
+++ client/src/components/Objectives.js
@@ -6,40 +6,47 @@ function Objectives() {
     <>
       <div className="ObjectiveDiv">
         <div className="objective-container">
-          <div className="row">
+          <div className="objective-row">
             <div className="col objective-box">
               <h1 className="values-title">Our Core Values</h1>
               <span className="core-values-span">
                 <p className="core-p">
                   <span className="core-title">EMPOWERMENT</span>
                   <br />
-                  The foundation focuses on empowering people and enabling their
-                  self-determination to live life in a responsible way.
-                  Corroborating confidence and helping people overcome the sense
-                  of powerlessness is the core value of Starts foundation.
+                  <span className="core-span">
+                    The foundation focuses on empowering people and enabling
+                    their self-determination to live life in a responsible way.
+                    Corroborating confidence and helping people overcome the
+                    sense of powerlessness is the core value of Starts
+                    foundation.
+                  </span>
                   <br />
                   <br />
                   <span className="core-title">
                     TRANSPARENCY AND ACCOUNTABILITY
                   </span>
                   <br />
-                  It has always maintained a transparent relationship with the
-                  development partners, community group, government, and people
-                  associated all the while fostering the bond in a significant
-                  way. Standing accountable to all the terms, it has paved its
-                  way into society as one of the emerging social organizations
-                  which works to thrive with dignity.
+                  <span className="core-span">
+                    It has always maintained a transparent relationship with the
+                    development partners, community group, government, and
+                    people associated all the while fostering the bond in a
+                    significant way. Standing accountable to all the terms, it
+                    has paved its way into society as one of the emerging social
+                    organizations which works to thrive with dignity.
+                  </span>
                   <br />
                   <br />
                   <span className="core-title">
                     ENSURING PRODUCTIVITY THROUGH RESOURCE UTILIZATION
                   </span>
                   <br />
-                  Starts foundation has worked its way through enumerous hurdles
-                  abiding by the vow of sustenance. It has helped people with
-                  provision of resources aiding them with physical
-                  infrastructures and skillful programmes ensuring their
-                  productivity and creativity.
+                  <span className="core-span">
+                    Starts foundation has worked its way through enumerous
+                    hurdles abiding by the vow of sustenance. It has helped
+                    people with provision of resources aiding them with physical
+                    infrastructures and skillful programmes ensuring their
+                    productivity and creativity.
+                  </span>
                 </p>
               </span>
             </div>
