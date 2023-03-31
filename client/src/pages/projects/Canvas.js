7e60f2284b2c016581afc4239c4ceac43873d1b4
yianan261 <yian.chen0131@gmail.com>
2023-03-31 15:53:38 -0400

    added SEO

--- client/src/pages/projects/Canvas.js
+++ client/src/pages/projects/Canvas.js
@@ -3,6 +3,13 @@ import "../../styles/Projects.css"
 import Layout from "../../components/Layout"
 import CanvasContentWriting from "../../components/CanvasContentWriting"
 import CanvasTopContainer from "../../components/CanvasTopContainer"
+import { SEO } from "../../components/SEO"
+
+/** SEO section */
+const title = "Project Canvas"
+const content =
+  "Starts foundation project for humanitarian work to support marginalized and underserved communities in Nepal"
+export const Head = () => <SEO title={title} content={content} />
 
 /**
  * /Projects/canvas page
