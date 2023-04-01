ce4acdcac7a78c03615d2c63fbbfe14f7e050ac3
amandaay <amandaay.aysma@gmail.com>
2023-03-31 18:16:38 -0700

    fixed some responsiveness

--- client/src/components/LifeTopContainer.js
+++ client/src/components/LifeTopContainer.js
@@ -32,7 +32,7 @@ function LifeTopContainer() {
           imagesGallery {
             _key
             asset {
-              gatsbyImageData(placeholder: BLURRED, fit: FILL)
+              gatsbyImageData(placeholder: BLURRED,  layout: CONSTRAINED)
               altText
             }
           }
@@ -58,7 +58,7 @@ function LifeTopContainer() {
         Project - {project === "SMILE" ? "A WAITING SMILE" : project}
       </h1>
       <div className="row proj-top-row">
-        <div className="col proj-top-left">
+        <div className="col-xs-12 col-sm-12 col-md-8 col-lg-6 proj-top-left">
           <ProjectPagination
             currentPage={page}
             totalPages={pageSize}
@@ -70,7 +70,7 @@ function LifeTopContainer() {
             
           </ProjectPagination>
         </div>
-        <div className="col proj-top-right">
+        <div className="col-xs-12 col-sm-12 col-md-4 col-lg-6 proj-top-right">
           <HandleText description={para} />
         </div>
       </div>
