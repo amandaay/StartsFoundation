736ddc4117c5442bc4000568a8ebbd076efab89f
kasivisu4 <kasivisu4@gmail.com>
2023-04-01 09:01:28 -0700

    Resolved conflicts in contact us page

--- client/src/pages/Volunteer.js
+++ client/src/pages/Volunteer.js
@@ -1,7 +1,15 @@
-import React, { useState } from "react"
+import React from "react"
 import Layout from "../components/Layout"
 import * as styles from "../styles/Volunteer.module.css"
+import { SEO } from "../components/SEO"
 
+/**
+ * SEO section
+ */
+const title = "Starts Foundation Volunteer"
+const content =
+  "Become a Volunteer at Starts Foundation and support their humanitarian work"
+export const Head = () => <SEO title={title} content={content} />
 export default function Volunteer() {
   return (
     <Layout>
@@ -12,9 +20,6 @@ export default function Volunteer() {
               src="https://docs.google.com/forms/d/e/1FAIpQLSfU-6AxrgGSPOmDJX1dfPl1e1yiZW8ehC6dHljx9HWlIcr-rg/viewform?embedded=true"
               width="100%"
               height="100%"
-              frameborder="0"
-              marginheight="0"
-              marginwidth="0"
             >
               {" "}
               Loading…
